8e376117a4d942fcfe7c7b1136bf858adde3c5ad
Татьяна <tanika135@mail.ru>
2024-10-29 23:11:29 +0300

    класс доработан,для билетов генерируется баркод

--- lib/tickets.php
+++ lib/tickets.php
@@ -1,8 +1,11 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"] . '/lib/api.php');
 
 class Tickets
 {
     private $connection;
+
+    const BARCODE_LENGTH = 8;
 
     public function __construct(Dataprovider $connection)
     {
@@ -26,27 +29,105 @@ class Tickets
     }
 
     /**
-     * @param int $eventId
-     * @param int $userId
-     * @param string $ticketType
+     * @param int $orderId
+     * @param array $ticketType
      * @param string $barcode
      * @return void
      * Сохраняет данные о билете в БД
      */
-    public function saveTicket (int $orderId, string $ticketType, string $barcode)
+    public function saveTickets(int $eventId, int $orderId, array $types)
     {
-        $query = "INSERT INTO tickets VALUES (
+        foreach ($types as $type => $quantity) {
+            if ($quantity != 0) {
+                for ($i = 0; $i < $quantity; $i++) {
+                    $this->saveTicket($eventId, $orderId, $type);
+                }
+            }
+        }
+    }
+
+    public function saveTicket (int $eventId, int $orderId, string $type)
+    {
+        try {
+            $code = $this->getBarcode($eventId, $type, self::BARCODE_LENGTH);
+
+            $query = "INSERT INTO tickets VALUES (
                                     NULL, 
                                     :order,
                                     :ticketType,
                                     :barcode)";
+            $tickets = $this->connection->getConnection()->prepare($query);
+            $tickets->execute(
+                [
+                    "order" => $orderId,
+                    "ticketType" => $type,
+                    "barcode" => $code,
+                ],
+            );
+
+        } catch (Exception $e) {
+            echo $e;
+            return false;
+        }
+        return true;
+    }
+
+    public function getBarcode(int $eventId, $type, int $length = 8)
+    {
+        $foundRemote = false;
+
+        $event = new Events($this->connection);
+        $eventData = $event->getEvent($eventId);
+
+        $api = new Api();
+
+        $adultQuantity = $type == 'ticket_adult' ? 1 : 0;
+        $kidQuantity = $type == 'ticket_kid' ? 1 : 0;
+
+        do {
+            $code = $this->generateBarcode($length);
+
+            $foundLocal = $this->searchBarcodeLocal($code);
+            if ($foundLocal)
+                continue;
+
+            $foundRemote = !$api->book($eventId, $eventData['event_date'], (int) $eventData['ticket_adult_price'],
+                $adultQuantity, (int) $eventData['ticket_kid_price'], $kidQuantity, $code);
+
+            echo $foundRemote;
+
+        } while($foundRemote);
+
+        if ($api->approve($code)) {
+            return $code;
+        } else {
+            throw new Exception($api->message);
+        }
+    }
+
+    public function searchBarcodeLocal(string $barcode)
+    {
+        $query = "SELECT * FROM tickets WHERE barcode = :barcode LIMIT 1" ;
         $tickets = $this->connection->getConnection()->prepare($query);
         $tickets->execute(
             [
-                "order" => $orderId,
-                "ticketType" => $ticketType,
                 "barcode" => $barcode,
             ],
         );
+        return $tickets->fetch(PDO::FETCH_COLUMN);
     }
+
+    public function generateBarcode(int $length = 8)
+    {
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+
 }
